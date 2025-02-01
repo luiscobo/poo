5ca6995cebdcb90f3ab01c9452785353b8b06f55
Luis Cobo <luis.cobo@gmail.com>
2025-02-01 10:08:35 -0500

    Update test01.py

--- test01.py
+++ test01.py
@@ -2,4 +2,21 @@
 def promedio(a, b):
     return (a + b) / 2
 
-print("Hola todos")
+# Import
+import unittest
+class TestCalculator2(unittest.TestCase):
+
+    def test_sub(self):
+        '''Test case function for subtraction'''
+        self.calc = Calculator()
+        result = self.calc.sub(10, 5)
+        expected = 5
+        self.assertEqual(result, expected)
+
+    def test_mul(self):
+        '''Test case function for multiplication'''
+        self.calc = Calculator()
+        result = self.calc.mul(3, 7)
+        expected = 21
+        self.assertEqual(result, expected)
+        print("Hola")
